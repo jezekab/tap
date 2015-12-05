33a758e343d4d3d62c2811e732128df5d18926d7
jezekab <jezekab@gmail.com>
2015-12-05 16:07:20 +1000

    added functionality to change colour

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 $(function () {
+
   //maths to figure out how many on screen
   var w = window.innerWidth;
   var h = window.innerHeight;
+  var cells_per_row = 10;
+  //var cells_per_col = h / cells_per_row
 
   //set initial values
   var mode = 1;
@@ -11,29 +14,65 @@ $(function () {
   var hsl;
 
   //generate cells to screen
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < cells_per_row; i++) {
     $("body").append("<div>");
   }
 
   //determine cell size
-  var cell_size = w / 10;
+  var cell_size = w / cells_per_row;
   var cell_size_css = cell_size + "px";
   $("div").css( "width", cell_size_css );
   $("div").css( "height", cell_size_css );
 
+  //to browser size on resize
+  $(window).resize(function () {
+    alert("You will lose your changes");
+  });
+
+  //change color mode
+  $( "#H" ).click(function() {
+      mode = 1;
+      alert(mode);
+  });
+
+  $( "#S" ).click(function() {
+      mode = 2;
+      alert(mode);
+  });
+
+  $( "#B" ).click(function() {
+      mode = 3;
+      alert(mode);
+  });
+
   //on cell click
   $( "div" ).click(function() {
     if (mode == 1) {
-      hue += 60;
+      if (hue > 360 - 36) {
+        hue = 0;
+      } else {
+        hue += 36
+      }
+      hue += 36;
     } else if (mode == 2) {
-      sat += 60;
-    } else {
-      bri += 60
+      if (sat > 90) {
+        sat = 0;
+      } else {
+        sat += 10
+      }
+    } else if (mode == 3){
+      if (bri > 90) {
+        bri = 0;
+      } else {
+        bri += 10
+      }
     }
 
     hsl = "hsl(" + hue + "," + sat + "% ," + bri + "%)";
     $(this).css( "background-color", hsl );
   });
+
+
 
   //on change of mode
 
